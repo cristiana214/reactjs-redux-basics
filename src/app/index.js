6fc9744682c7bacfc0a05161761dee206b75f32d
Cristiana Chavez <cristianachavez214@gmail.com>
2018-06-21 19:22:09 +0800

    state and immutability

--- src/app/index.js
+++ src/app/index.js
@@ -1,35 +1,47 @@
-import {render} from "react-dom";
-import React from "react";
-import {createStore} from "redux";
+import {createStore} from 'redux';
 
-const reducer = (state, action) => {
-    switch (action.type) {
+const initialState = {
+    reduct:1,
+    lastValues: []
+};
+
+const reducer = (state,action) => {
+    switch(action.type) {
         case "ADD":
-            state = state + action.payload;
+            state = {
+                ...state,
+                result:state.result + action.payload,
+                lastValues:[...state.lastValues,action.payload]
+            };
             break;
         case "SUBTRACT":
-            state = state - action.payload;
+            state = {
+                ...state,
+                result:state.result - action.payload,
+                lastValues:[...state.lastValues,action.payload]
+            };
             break;
     }
     return state;
-};
-
+}
 
 const store = createStore(reducer, 1);
 
 store.subscribe(() => {
-    console.log("Store updated!", store.getState());
+    console.log("STORE updated", store.getState());
 });
 
 store.dispatch({
-    type: "ADD",
-    payload: 100
+   type:"ADD",
+   payload:100 
 });
+
 store.dispatch({
-    type: "ADD",
-    payload: 22
-});
+    type:"ADD",
+    payload:22 
+ });
+ 
 store.dispatch({
-    type: "SUBTRACT",
-    payload: 80
-});
+    type:"SUBTRACT",
+    payload:10 
+ });
