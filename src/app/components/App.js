6196bb0367b54c23556a130752bce86bcf3d4845
Cristiana Chavez <cristianachavez214@gmail.com>
2018-06-22 17:14:11 +0800

    update

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,36 +1,28 @@
-import React from "react";
-import {connect} from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import { User } from './User';
-import { Main } from './Main';
 
-class App extends React.Component {
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
     render() {
         return (
-            <div className="container">
-                <Main changeUsername={() => this.props.setName("Anna")}/>
-                <User username={this.props.user.name}/>
+            <div>
+                
             </div>
         );
     }
 }
 
-const mapStateToProps = (state) => {
-  return {
-      user: state.user,
-      math: state.math
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        setName: (name) => {
-            dispatch({
-                type: "SET_NAME",
-                payload: name
-            });
-        }
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+    mapStateToProps,
+)(App);
